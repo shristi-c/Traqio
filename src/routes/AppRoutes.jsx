3ccda33ac7ab764e5ffc189220889f89adb1fd97
shristi-c <loginshristi@gmail.com>
2026-07-09 16:07:11 +0530

    Initial project setup with React, Tailwind, Firebase and routing

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Landing from "../pages/Landing/Landing";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+import Dashboard from "../pages/Dashboard/Dashboard";
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
